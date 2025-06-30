3ad44cc9bddc9a9a195ddbcf779caabeddbf1cae
José Manuel <josemanuelharri@gmail.com>
2025-06-30 19:26:11 -0400

    se borra pizza card de app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,6 @@
 import './App.css'
 import Footer from './components/Footer'
 import Navbar from './components/Navbar'
-import CardPizza from './components/CardPizza'
 import Home from './components/Home'
 import Register from './components/Register'
 import Login from './components/Login'
